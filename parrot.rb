d300f56f156ced929de7ac7ef11863e9d4eda0da
Shahwar Spall <shevy.spall@gmail.com>
2019-07-16 03:52:08 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-name= []
-def parrot (name,default="Squawk!")
-  puts {name}
+name=[]
+def parrot(name,default="Squawk!")
+  puts{name}
 end
 parrot
 
